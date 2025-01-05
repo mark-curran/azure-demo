953af1f6628a32245d970b3b2436f8fb87281107
Mark Curran <markcurran@Marks-MacBook-Air.local>
2025-01-05 14:57:00 +1100

    Modify aks cluster outputs.

--- terraform/modules/aks_cluster/outputs.tf
+++ terraform/modules/aks_cluster/outputs.tf
@@ -5,8 +5,3 @@ output "resource_group_name" {
 output "cluster_name" {
   value = azurerm_kubernetes_cluster.default.name
 }
-
-output "kube_config_raw" {
-  value = azurerm_kubernetes_cluster.default.kube_config_raw
-  sensitive = true
-}
